85bf5efdec211725ab5cd51901c45c1f10e8fe36
ms-tech-geek <thecorporator@gmail.com>
2024-10-04 11:30:52 +0530

    add internal links

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,12 @@
 const HomePage = () => {
-    return <h1>My Home Page</h1>
-}
+	return (
+		<>
+			<h1>My Home Page</h1>
+			<p>
+				Go to <a href="/products">the list of products</a>
+			</p>
+		</>
+	);
+};
 
 export default HomePage;
