6cf98d77793ce3b8c03121f83a07073c6f3ec6de
ms-tech-geek <thecorporator@gmail.com>
2024-10-04 11:24:26 +0530

    rollback to previous routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import {
 	createBrowserRouter,
-	createRoutesFromElements,
-	Route,
+	// createRoutesFromElements,
+	// Route,
 	RouterProvider,
-	Routes,
+	// Routes,
 } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 
-const routeDefinations = createRoutesFromElements(
-	<Routes>
-		<Route path="/" element={<HomePage />} />
-		<Route path="/products" element={<ProductsPage />} />
-	</Routes>
-);
+// const routeDefinations = createRoutesFromElements(
+// 	<Routes>
+// 		<Route path="/" element={<HomePage />} />
+// 		<Route path="/products" element={<ProductsPage />} />
+// 	</Routes>
+// );
 
-// const router = createBrowserRouter([
-// 	{ path: "/", element: <HomePage /> },
-// 	{ path: "/products", element: <ProductsPage /> },
-// ]);
+const router = createBrowserRouter([
+	{ path: "/", element: <HomePage /> },
+	{ path: "/products", element: <ProductsPage /> },
+]);
 
-const router = createBrowserRouter(routeDefinations);
+// const router = createBrowserRouter(routeDefinations);
 
 function App() {
 	return <RouterProvider router={router} />;
