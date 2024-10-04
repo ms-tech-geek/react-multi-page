026df4c887ea335115334f8624393bffabf99dc6
ms-tech-geek <thecorporator@gmail.com>
2024-10-04 11:54:05 +0530

    add some basic styling to react components

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
-import MainNavigation  from "../components/MainNavigation";
+import MainNavigation from "../components/MainNavigation";
+import { classes } from "./Root.module.css";
 
 const RootLayout = () => {
 	return (
 		<>
 			<MainNavigation />
-			<Outlet />
+			<main className={classes.content}>
+				<Outlet />
+			</main>
 		</>
 	);
 };
