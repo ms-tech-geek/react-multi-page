736cd3354a57d1cef483845c3618f0212704126c
ms-tech-geek <thecorporator@gmail.com>
2024-10-04 16:40:02 +0530

    cleanup

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
-import classes from "./Root.module.css";
 
 const RootLayout = () => {
 	return (
