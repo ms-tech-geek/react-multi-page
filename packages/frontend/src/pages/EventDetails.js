56b1894a83b8b7abedb16e1132cb4a0b12d99a91
ms-tech-geek <thecorporator@gmail.com>
2024-10-10 02:32:25 +0530

    output the id of selected event on event detail page

--- packages/frontend/src/pages/EventDetails.js
+++ packages/frontend/src/pages/EventDetails.js
@@ -1,5 +1,13 @@
+import { useParams } from "react-router-dom";
+
 const EventsDetailsPage = () => {
-    return <h1>Events Details Page</h1>
-}
+	const params = useParams();
+	return (
+		<>
+			<h1>Events Details Page</h1>
+			<p>Event ID : {params.eventId}</p>
+		</>
+	);
+};
 
 export default EventsDetailsPage;
