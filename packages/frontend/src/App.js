704ed573fb195566539d7d25f92253a73004e36f
ms-tech-geek <thecorporator@gmail.com>
2024-10-10 01:39:23 +0530

    add routing and route definations for page components

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,4 +1,10 @@
 // Challenge / Exercise
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/Home";
+import EventsPage from "./pages/Events";
+import EventDetailPage from "./pages/EventDetails";
+import NewEventPage from "./pages/NewEvent";
+import EditEventPage from "./pages/EditEvent";
 
 // 1. Add five new (dummy) page components (content can be simple <h1> elements)
 //    - HomePage
@@ -20,8 +26,16 @@
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
+const router = createBrowserRouter([
+	{ path: "/", element: <HomePage /> },
+	{ path: "/events", element: <EventsPage /> },
+	{ path: "/events/:eventId", element: <EventDetailPage /> },
+	{ path: "/events/new", element: <NewEventPage /> },
+	{ path: "/events/:eventId/edit", element: <EditEventPage /> },
+]);
+
 function App() {
-  return <div></div>;
+	return <RouterProvider router={router} />;
 }
 
 export default App;
