3aa2e3e7541ad48c2d32a81740e9726f8b7ea7db
ms-tech-geek <thecorporator@gmail.com>
2024-10-10 01:51:49 +0530

    add properly working links to main navigation

--- packages/frontend/src/components/MainNavigation.js
+++ packages/frontend/src/components/MainNavigation.js
@@ -1,20 +1,21 @@
-import classes from './MainNavigation.module.css';
+import { Link } from "react-router-dom";
+import classes from "./MainNavigation.module.css";
 
 function MainNavigation() {
-  return (
-    <header className={classes.header}>
-      <nav>
-        <ul className={classes.list}>
-          <li>
-            <a>Home</a>
-          </li>
-          <li>
-            <a>Events</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+	return (
+		<header className={classes.header}>
+			<nav>
+				<ul className={classes.list}>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/events">Events</Link>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
 }
 
 export default MainNavigation;
