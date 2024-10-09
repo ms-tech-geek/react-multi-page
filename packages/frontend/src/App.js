ba222faff18607373c9e02dc9879ae63ce9640d6
ms-tech-geek <thecorporator@gmail.com>
2024-10-10 01:50:00 +0530

    reconfigure router to add page routes under root layout

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,5 +1,6 @@
 // Challenge / Exercise
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootLayout from "./layouts/Root";
 import HomePage from "./pages/Home";
 import EventsPage from "./pages/Events";
 import EventDetailPage from "./pages/EventDetails";
@@ -26,13 +27,17 @@ import EditEventPage from "./pages/EditEvent";
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
-const router = createBrowserRouter([
-	{ path: "/", element: <HomePage /> },
-	{ path: "/events", element: <EventsPage /> },
-	{ path: "/events/:eventId", element: <EventDetailPage /> },
-	{ path: "/events/new", element: <NewEventPage /> },
-	{ path: "/events/:eventId/edit", element: <EditEventPage /> },
-]);
+const router = createBrowserRouter([{
+	path: "/",
+	element: <RootLayout />,
+	children: [
+		{ index: true, element: <HomePage /> },
+		{ path: "/events", element: <EventsPage /> },
+		{ path: "/events/:eventId", element: <EventDetailPage /> },
+		{ path: "/events/new", element: <NewEventPage /> },
+		{ path: "/events/:eventId/edit", element: <EditEventPage /> },
+	],
+}]);
 
 function App() {
 	return <RouterProvider router={router} />;
