0ed2d7e6e04f52c4e6518c0133768bbb9d7f04bd
ms-tech-geek <thecorporator@gmail.com>
2024-10-10 20:20:28 +0530

    refactor code and store loader in component

--- packages/frontend/src/pages/Events.js
+++ packages/frontend/src/pages/Events.js
@@ -1,6 +1,16 @@
 import { useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
+export const loader = async () => {
+	const response = await fetch("http://localhost:8080/events");
+	if (!response.ok) {
+		// ...
+	} else {
+		const resData = await response.json();
+		return resData.events;
+	}
+}
+
 const EventsPage = () => {
 	const events = useLoaderData();
 
