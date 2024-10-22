0912047f94f35613471ebcd8c2fd2daf100753c1
ms-tech-geek <thecorporator@gmail.com>
2024-10-22 20:46:17 +0530

    json() utility function

--- packages/frontend/src/pages/Events.js
+++ packages/frontend/src/pages/Events.js
@@ -1,4 +1,4 @@
-import { useLoaderData } from "react-router-dom";
+import { json, useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 const EventsPage = () => {
@@ -13,9 +13,7 @@ export default EventsPage;
 export const loader = async () => {
 	const response = await fetch("http://localhost:8080/events");
 	if (!response.ok) {
-		throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
-			status: 500,
-		});
+		throw json({ message: "Could not fetch events." }, { status: 500 });
 	} else {
 		return response;
 	}
